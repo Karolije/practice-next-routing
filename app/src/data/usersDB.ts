22d6cab9df9152c3a0f200bb1bb295d9f87a22bc
Karolina <karolina.jedrajczyk@gmail.com>
2025-09-04 11:49:43 +0200

    poprawki

--- app/src/data/usersDB.ts
+++
@@ -1,8 +0,0 @@
-export const db = {
-    users: [
-      { id: 1, name: "Anna Kowalska", email: "anna@example.com" },
-      { id: 2, name: "Jan Nowak", email: "jan@example.com" },
-    ],
-    nextId: 3,
-  };
-  
